9d811c2dce613d0f8d38aac1a71385d00ac889d7
mhertsatinyan <mtsatinyan@technologist.ai>
2023-08-27 16:28:50 +0400

    build the basic routes and controllers

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes/authRoutes')
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('worked')
+})
+
+app.use(routes)
+
+const dbUrl = 'mongodb+srv://leonmher:mypass123@cluster0.kc6el.mongodb.net/node-auth'
+
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000, () => console.log('listening on port 3000')))
+    .catch((err) => console.log(err))
+
+
---
+++ controller/authController.js
@@ -0,0 +1,16 @@
+module.exports.signup_get = (req, res) => {
+    console.log('signup')
+    res.send('signed up')
+}
+
+module.exports.login_get = (req, res) => {
+    console.log('login')
+}
+
+module.exports.signup_post = (req, res) => {
+    console.log('signup post')
+}
+
+module.exports.login_post = (req, res) => {
+    console.log('login post')
+}
